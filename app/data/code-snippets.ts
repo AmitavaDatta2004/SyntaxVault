a0115887fc243997597fdd7dfd2f0ae1ba96fd09
Amitava <dattaarup2004@gmail.com>
2024-11-29 13:24:45 +0530

    day 6

--- app/data/code-snippets.ts
+++ app/data/code-snippets.ts
@@ -583,6 +583,52 @@ class Solution:
 
     `,
     },
+    {
+        id: '9',
+        title: 'Day 6 - Majority Element II',
+        description: 'You are given an array of integer arr[] where each number represents a vote to a candidate. Return the candidates that have votes greater than one-third of the total votes, If there\'s not a majority vote, return an empty array.',
+        language: 'python',
+        category: 'GFG160',
+        code: `
+class Solution:
+    def findMajority(self, arr):
+        # First Pass: Find potential candidates
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+        
+        for num in arr:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        # Second Pass: Verify candidates
+        count1, count2 = 0, 0
+        for num in arr:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+        
+        # Check if candidates meet the n/3 criterion
+        result = []
+        n = len(arr)
+        if count1 > n // 3:
+            result.append(candidate1)
+        if count2 > n // 3:
+            result.append(candidate2)
+        
+        return sorted(result)
+
+    `,
+    },
 
 
 
